d5983c78e654029fec8e95b3e940c92341153278
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2017-12-12 14:05:11 +0100

    Add axios and get request to event controller

--- controllers/event.js
+++ controllers/event.js
@@ -1,17 +1,16 @@
 const Event = require('../models/Event');
 const http = require('http');
+const axios = require('axios');
+
 
 exports.getAll = (req, res) => {
   Event.find((err, docs) => {
-    let velibs = '';
-    let data = http.get('http://www.velostanlib.fr/service/carto', (resp) => {
-      let data = '';
-      resp.on('data', (chunk)=>{
-        velibs +=chunk;
-      });
-      resp.on('end',()=>{
-        res.render('account/map', {title : 'Events',"events": docs, "velibs" : velibs });
-      })
+
+    axios.get('http://www.velostanlib.fr/service/carto').then((response) => {
+      res.render('account/map', { title: 'Events', events: docs, velibs: response.data });
+    }).catch((error) => {
+      console.log(error);
     });
+
   });
 };
