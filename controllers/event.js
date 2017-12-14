2d20dc49063703f607d03c91f5014daa2ad7d0ec
RENOUARD Quentin <quentin.renouard9@etu.univ-lorraine.fr>
2017-12-14 15:43:38 +0100

    FInish database refresh funtcions

--- controllers/event.js
+++ controllers/event.js
@@ -1,13 +1,11 @@
 const Event = require('../models/Event');
-const axios = require('axios');
-const parser = require('xml2json');
+
+exports.dropEvents = (req, res) => {
+  Event.remove({}, () => {});
+};
 
 exports.getAll = (req, res) => {
   Event.find((err, docs) => {
-    axios.get('http://www.velostanlib.fr/service/carto').then((response) => {
-      res.render('account/map', { title: 'Events', events: docs, velibs: parser.toJson(response.data)});
-    }).catch((error) => {
-      console.log(error);
-    });
+    res.render('map', { title: 'Events', events: docs });
   });
 };
--- controllers/map.js
+++ controllers/map.js
@@ -2,11 +2,12 @@ const Ad = require('../models/Ad');
 
 
 exports.map = (req, res) => {
-    res.render('account/map', {title : 'map'});
+  res.render('account/map', { title: 'map' });
 };
 
 exports.getAll = (req, res) => {
-   Ad.find((err, docs) => {
-      res.render('account/map', {title : 'Map',"ads": docs});
-    }).sort({'createdAt': -1});
+  Ad.find((err, docs) => {
+    res.render('account/map', { title: 'Map', ads: docs });
+  }).sort({ createdAt: -1 });
 };
+
