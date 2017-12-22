6fa1288dc5a41ea5a0a1dad1e3c2617eec3af87e
lucandreiut <luc.andre4@etu.univ-lorraine.fr>
2017-12-22 05:30:34 +0100

    backup avant pull

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,7 @@
 let map;
 let marker;
+let vliboul = true;
+let parkingboul = true;
 const markers = [];
 const markersVelib = [];
 const markersEvent = [];
@@ -11,6 +13,31 @@ const velibs = velibPug;
 const parkings = parkingPug;
 console.log(parkings);
 var controlUI;
+const searchbar = document.getElementById('pac-input');
+
+searchbar.addEventListener("keypress", keyPressed);
+
+function keyPressed(k) {
+  if (k.code == 'Enter'){ 
+	//let i = 0;
+	  for(let evt in events){
+		if(searchbar.value == events[evt].title){
+			let pos = {
+				lat: events[evt].lat,
+				lng: events[evt].lng
+			};
+			map.panTo(pos);
+			//let infowindow = new google.maps.InfoWindow();
+			//let html = '<h1>'+events[evt].title+'</h1> <p>'+events[evt].date+'</p><p>'+events[evt].description+"</p><p><a href='' onclick='render(event,\""+events[evt].title+"\")'>Comment m'y rendre ?</a></p>";
+			//infowindow.setContent(html);			
+			//infowindow.open(map, markersEvent[i]);
+		}
+		//i++;
+	  }
+  }
+  return false;  // no propagation or default
+}
+
 
 function openNav() {
       if(menu == 0){
@@ -59,28 +86,36 @@ function CenterControl(controlDiv, map) {
 }
 
 function removeVelibs(){
-	let chk_ceil= document.getElementById("vlib");       
-    if(chk_ceil.checked == true){
+	let vliblink = document.getElementById('vlib');	
+    if(vliboul == true){
 		for(let i in markersVelib){
 			markersVelib[i].setMap(null);
+			vliboul = false;
+			vliblink.setAttribute("class", "clicked");
 		}
     }else{
 		for(let i in markersVelib){
 			markersVelib[i].setMap(map);
+			vliboul = true;
+			vliblink.setAttribute("class", "unclicked");
 		}
 	}
 
 }
 
 function removeParkings(){
-	let chk_ceil= document.getElementById("park");       
-    if(chk_ceil.checked == true){
+	let pklink = document.getElementById('prking');		
+    if(parkingboul == true){
 		for(let i in markersParking){
 			markersParking[i].setMap(null);
+			parkingboul = false;
+			pklink.setAttribute("class", "clicked");
 		}
     }else{
 		for(let i in markersEvent){
 			markersParking[i].setMap(map);
+			parkingboul = true;
+			pklink.setAttribute("class", "unclicked");
 		}
 	}
 
@@ -93,6 +128,10 @@ function bindInfoWindow(marker, map, infowindow, html) {
     });
 } 
 
+function render(e, n) {
+	e.preventDefault();
+	alert(n);
+} 
 
 function initialize() {
 	geocoder.geocode({'address': 'Nancy'}, function(results, status) {
@@ -104,6 +143,7 @@ function initialize() {
 			  mapTypeControl: false,
 			  streetViewControl: false,
 			  scrollwheel: false,
+			  fullscreenControl: false,
 			  disableDoubleClickZoom: true,
 			  styles : [
 						  {
@@ -146,7 +186,7 @@ function initialize() {
 					//console.log(velibs[mark].lat);
 					var infowindow = new google.maps.InfoWindow();
 					var nametransfo = velibs[mark].name.replace(/[0-9]/g, '').replace(/-/g, "").replace(/\s*\(.*?\)\s*/g, '').slice( 2 );
-					var html = '<h1>'+nametransfo+'</h1> <p> info </p>'
+					var html = '<h1>'+nametransfo+'</h1> <p>'+ velibs[mark].address +'</p><p>Nombre de vélos :'+ velibs[mark].number +"</p><p><a href='' onclick='render(event,\""+nametransfo+"\")'>Comment m'y rendre ?</a></p>";
 					marker = new google.maps.Marker({
 						map: map,
 						position: {lat: velibs[mark].lat, lng: velibs[mark].lng},
@@ -159,11 +199,12 @@ function initialize() {
 				
 				for(let evt in events){
 					var infowindow = new google.maps.InfoWindow();
-					var html = '<h1>'+events[evt].title+'</h1> <p> info </p>'
+					var html = '<h1>'+events[evt].title+'</h1> <p>'+events[evt].date+'</p><p>'+events[evt].description+"</p><p><a href='' onclick='render(event,\""+events[evt].title+"\")'>Comment m'y rendre ?</a></p>";
 					marker = new google.maps.Marker({
 						map: map,
 						position: {lat: events[evt].lat, lng: events[evt].lng},
-						title: events[evt].title
+						title: events[evt].title,
+						icon: '../img/event.png'
 					});
 					bindInfoWindow(marker, map, infowindow, html);
 					markersEvent.push(marker);
@@ -174,134 +215,27 @@ function initialize() {
 					marker = new google.maps.Marker({
 						map: map,
 						position: {lat: parkings[park].lat, lng: parkings[park].lng},
-						title: parkings[park].title
+						title: parkings[park].title,
+						icon: '../img/parking.png'
 					});
 					bindInfoWindow(marker, map, infowindow, html);
 					markersParking.push(marker);
 				}
+				if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition(function(position) {
+						marker = new google.maps.Marker({
+							map: map,
+							position: {lat: position.coords.latitude, lng: position.coords.longitude},
+							animation: google.maps.Animation.BOUNCE,
+							title: "Vous êtes ici",
+							icon: '../img/ici.png'
+						});
+					});
+					
+				}
 			};
 		}
 	});
-  
- // if(document.getElementById("map-canvas")){
-//	  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-//	};
-	 // searchAddress();
-	
-	if(document.getElementById('infos')){
-	
-		const infoWindow = new google.maps.InfoWindow({map: map});
-
-        // Try HTML5 geolocation.
-		
-			if (navigator.geolocation) {
-			  navigator.geolocation.getCurrentPosition(function(position) {
-				let pos = {
-					lat: position.coords.latitude,
-					lng: position.coords.longitude
-				};
-
-				infoWindow.setPosition(pos);
-				infoWindow.setContent("You're here!");
-				map.setCenter(pos);
-				map.setZoom(9);
-			  }, function() {
-				handleLocationError(true, infoWindow, map.getCenter());
-			  });
-			} else {
-			  // Browser doesn't support Geolocation
-			  handleLocationError(false, infoWindow, map.getCenter());
-			}
-		}
-//	}	
 }
 
 google.maps.event.addDomListener(window, "load", initialize);
-
-//AJAX with javascript
-function ajaxGet(url, callback) {
-    let req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Erreur réseau avec l'URL " + url);
-    });
-    req.send(null);
-}
-
-function searchAddress() {
-	
-	if(document.getElementById('adress')){
-	  var addressInput = document.getElementById('adress').innerHTML;
-	  var geocoder = new google.maps.Geocoder();
-	  
-	  geocoder.geocode({address: addressInput}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-		  var myResult = results[0].geometry.location; // reference LatLng value
-		  createMarker(myResult); // call the function that adds the marker
-		  map.setCenter(myResult);
-		  map.setZoom(17);
-		}
-	  });
-	} else {		
-		var addresses = document.getElementById('infos').getElementsByClassName('addresses');
-		var names = document.getElementById('infos').getElementsByClassName('titles');
-		var des = document.getElementById('infos').getElementsByClassName('descr');
-		var img = document.getElementById('infos').getElementsByClassName('imgs');
-		var lks = document.getElementById('infos').getElementsByClassName('links');
-		var geocoder = new google.maps.Geocoder();
-		infoWindow = new google.maps.InfoWindow();
-		//var markers = [];
-			for(var i = 0; i < addresses.length; i++){
-				geocoder.geocode({address: addresses[i].value}, createMarkers(addresses[i].value, names[i].value, des[i].value, img[i].value, lks[i].value,));
-			}			
-	}
-}
-
-function createMarker(latlng) {
-
-   // If the user makes another search you must clear the marker variable
-   if(marker != undefined && marker != ''){
-    marker.setMap(null);
-    marker = '';
-   }
-
-   marker = new google.maps.Marker({
-      map: map,
-      position: latlng
-   });
-
-}
-
-
-function createMarkers(addr, name, descr, lk) {
-	
-	 return function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                title: name,
-            });
-			
-			var content='<h1>'+name+'</h1>' +	
-						'<h2>'+addr+'</h2>' +
-						'<p>'+descr+'</p>' +
-						'<p><a href="" title="'+name+'" target="_parent">How to get there</a></p>';
-
-			
-            (function (marker, addr) {
-                google.maps.event.addListener(marker, "click", function (e) {
-                    infoWindow.setContent(content);
-                    infoWindow.open(map, marker);
-                });
-            })(marker, addr);
-        }
-    };
-}
